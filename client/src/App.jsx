4aab1ef75528ea432b71c5c90802c9d7faf74467
Kevin Guo <keving@exchange.airg>
2018-06-15 10:52:58 -0700

    Airbnb standards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import FirstComponent from "./components/FirstComponent";
 
-const App = () => (
-  <div>
-    <h1>From React</h1>
-    <FirstComponent />
-  </div>
-);
+function App() {
+  return (
+    <div>
+      <h1>From React</h1>
+      <FirstComponent />
+    </div>
+  );
+}
 
 export default App;
