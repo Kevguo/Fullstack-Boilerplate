085f062f68fab552e0b4a778024240be3ec6a84e
kevguo <kevintheguo@gmail.com>
2018-06-23 17:07:13 -0700

    Deployment routes set up

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,18 @@
 import express from "express";
+import path from "path";
 
 const app = express();
-
-const PORT = 5000;
 
 app.get("/api/test", (req, res) => {
   res.json({ response: "From Server" });
 });
+
+app.use(express.static(path.join(__dirname, "../../client/build/")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
